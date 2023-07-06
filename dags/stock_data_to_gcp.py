c054b5136f6439e3f9deaefc5f1e6dd61f205ba6
justpqa <102847503+justpqa@users.noreply.github.com>
2023-07-06 17:42:11 -0500

    Update stock_data_to_gcp.py

--- dags/stock_data_to_gcp.py
+++ dags/stock_data_to_gcp.py
@@ -17,6 +17,7 @@ PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
 path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 dataset_file = "YOUR_DATASET_CSV_FILE"
+dataset_lst = ["YOUR_DATASET_" + str(i) + ".csv" for i in range(5)]
 dataset_name = "YOUR_BQ_DATASET"
 BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'YOUR_BQ_DATASET')
 table_name = "YOUR_BQ_TABLE"
@@ -29,24 +30,43 @@ def get_top500_companies():
 
     # Extract the required columns
     sp500_companies = sp500_table['Symbol'].tolist()
+    # remove error company ticker
     sp500_companies.remove("BF.B")
     sp500_companies.remove("BRK.B")
 
     return sp500_companies
 
-def get_top500_intraday():
-    # get the list of top 500 companies
-    company_lst = get_top500_companies()
-    # get the intraday data of these companies
-    data = yf.download(tickers=company_lst[0], period='1d', interval='60m', progress=False)
-    data["ticker"] = company_lst[0]
-    for i in range(1, len(company_lst)):
-        temp = yf.download(tickers=company_lst[i], period='1d', interval='60m', progress=False)
-        temp["ticker"] = company_lst[i]
-        data = pd.concat([data, temp], axis = 0)
-    data = data.iloc[:, [6, 0, 1, 2, 3, 4, 5]].reset_index()
-    data.to_csv(f"{path_to_local_home}/{dataset_file}", index = False)
+def get_all_intraday(inx):
+    if inx >= 0 and inx < 5:
+        company_lst = get_top500_companies()
+        company_lst = company_lst[100 * inx: 100 * (inx + 1)] if inx < 4 else company_lst[100 * inx:]
+        data = yf.download(tickers=company_lst[0], period='1m', interval='1m', progress=False)
+        for i in range(1, len(company_lst)):
+            temp = yf.download(tickers=company_lst[i], period='1m', interval='1m', progress=False)
+            temp["ticker"] = company_lst[i]
+            data = pd.concat([data, temp], axis = 0)
+        data = data.iloc[:, [6, 0, 1, 2, 3, 4, 5]].reset_index()
+        data["Datetime"] = data["Datetime"].apply(lambda x: x.to_pydatetime().replace(tzinfo = None))
+        data.to_csv(f"{path_to_local_home}/{dataset_lst[inx]}", index = False)
+        return
+    else:
+        print("Error in the index")
+        return
+
+def join_all_stocks(dlst, dfile):
+    if len(dlst) == 0:
+        print("The file list that you provide is empty")
     
+    res = pd.read_csv(f"{path_to_local_home}/{dlst[0]}")
+    
+    if len(dlst) > 1:
+        for i in range(1, len(dlst)):
+            temp = pd.read_csv(f"{path_to_local_home}/{dlst[i]}")
+            res = pd.concat([res, temp])
+    
+    res.to_csv(f"{path_to_local_home}/{dfile}", index = False)
+    return 
+
 # define the default arguments for dags
 default_args = {
     'owner': 'airflow',
@@ -61,17 +81,35 @@ default_args = {
 # define the dags
 with DAG(
     dag_id="data_ingestion_gcs_dag",
-    schedule_interval="*/5 * * * *",
+    schedule_interval=None,
     default_args=default_args,
     catchup=False,
     max_active_runs=1
 ) as dag:
     
-    get_stock_task = PythonOperator(
-        task_id = "get_stock",
-        python_callable = get_top500_intraday
-    )
+    intraday_task_lst = []
+    for task_inx in range(5):
+        task_name = "get_stock_" + str(task_inx)
+        new_task = PythonOperator(
+            task_id = task_name,
+            python_callable = get_all_intraday,
+            op_kwargs = {
+                "inx": task_inx
+            },
+            dag = dag
+        )
+        intraday_task_lst.append(new_task)
     
+    join_all_stocks_task = PythonOperator(
+        task_id = "joined_all_stocks",
+        python_callable = join_all_stocks,
+        op_kwargs = {
+            "dlst": dataset_lst,
+            "dfile": dataset_file
+        },
+        dag = dag
+    )
+
     local_to_gcs_task = LocalFilesystemToGCSOperator(
         task_id = "local_to_gcs",
         src = f"{path_to_local_home}/{dataset_file}",
@@ -98,4 +136,4 @@ with DAG(
         dag=dag,
     )
     
-get_stock_task >>  local_to_gcs_task >> gcs_to_bigquery_task
+intraday_task_lst >> join_all_stocks_task >>  local_to_gcs_task >> gcs_to_bigquery_task
